0cfb2b733c17c444eda4007c839c03ec456e594b
SharKPonG <Pingkiesdev@gmail.com>
2025-08-29 20:18:42 +0700

    Week6

--- Week6/BinarySearchVersion.cpp
+++ Week6/BinarySearchVersion.cpp
@@ -4,29 +4,32 @@ using namespace std;
 
 int main() {
     vector<int> students;
-    int num;
-    while (cin.peek() != '\n' && cin >> num) {
-        students.push_back(num);
-    }
 
+
+        int x;
+    while (cin >> x) {
+        students.push_back(x);
+        int ch = cin.peek();
+        if (ch == '\n' || ch == '\r' || ch == EOF) {
+            if (ch == '\r') { cin.get(); ch = cin.peek(); }
+        break;
+    }
+}
     int target;
-    cin >> target;
+    if (!(cin >> target)) return 0;
 
     int left = 0, right = students.size() - 1, rounds = 0;
     bool found = false;
 
+    
     while (left <= right) {
         rounds++;
         int mid = left + (right - left) / 2;
-        if (students[mid] == target) {
-            found = true;
-            break;
-        } else if (students[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
+        if (students[mid] == target) { found = true; break; }
+        else if (students[mid] < target) left = mid + 1;
+        else right = mid - 1;
     }
+
 
     if (found)
         cout << "Present - " << rounds << endl;
